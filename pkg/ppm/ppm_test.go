b9237ffc15fb69c0d806f0ddedbcd4166bb6aa0b
Daniel Vérité <dverite@gmail.com>
2025-05-13 09:24:36 +0200

    chore(lint): remove functions that are no longer used
    Signed-off-by: Daniel Vérité <dverite@gmail.com>

--- pkg/ppm/ppm_test.go
+++ pkg/ppm/ppm_test.go
@@ -7,35 +7,6 @@ import (
 	"github.com/qonto/postgresql-partition-manager/internal/infra/postgresql"
 )
 
-func getBoundFormat(config partition.Configuration) string {
-	var dateFormat string
-
-	switch config.Interval {
-	case partition.Daily, partition.Weekly:
-		dateFormat = "2006-01-02"
-	case partition.Monthly:
-		dateFormat = "2006-01"
-	case partition.Quarterly:
-		dateFormat = "2006-01"
-	case partition.Yearly:
-		dateFormat = "2006"
-	}
-
-	return dateFormat
-}
-
-func formatLowerBound(t *testing.T, p partition.Partition, config partition.Configuration) (output string) {
-	t.Helper()
-
-	return p.LowerBound.Format(getBoundFormat(config))
-}
-
-func formatUpperBound(t *testing.T, p partition.Partition, config partition.Configuration) (output string) {
-	t.Helper()
-
-	return p.UpperBound.Format(getBoundFormat(config))
-}
-
 func partitionResultToPartition(t *testing.T, partitions []partition.Partition, dateFormat string) (result []postgresql.PartitionResult) {
 	t.Helper()
 
